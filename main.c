bd2ec99a5a3cff06a9c3434cc1c54fab934f94be
劉俊廷 <M10407209@mail.ntust.edu.tw>
2015-08-20 23:56:38 +0800

    SAY HI

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: david
+ *
+ * Created on 2015年8月20日, 下午 11:54
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("HIHI");
+
+    return (EXIT_SUCCESS);
+}
+
